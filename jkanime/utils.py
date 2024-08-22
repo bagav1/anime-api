5eb1c5ae8a62356b1fc3f6971e5f9216a09d2031
Bryan Garcia <bragaru@hotmail.es>
2024-08-21 22:49:09 -0500

    Remove unused imports and obsolete parse_table function
    Eliminating unnecessary imports and the parse_table function streamlines the codebase. The removal helps reduce clutter, improving maintainability and readability. This also eliminates any confusion around the parsing functionality that is no longer in use, ensuring a cleaner implementation moving forward.

--- jkanime/utils.py
+++ jkanime/utils.py
@@ -1,6 +1,3 @@
-from bs4 import Tag
-from animeflv.exception import AnimeFLVParseError
-
 
 def removeprefix(str: str, prefix: str) -> str:
     """
@@ -28,26 +25,3 @@ def safe_strip(text: str) -> str:
     :return (str): The stripped string, or an empty string if the input is None.
     """
     return text.strip() if text is not None else ""
-
-
-def parse_table(table: Tag):
-    """
-    Parse a given HTML table into a list of dictionaries.
-
-    :param table (Tag): The HTML table to parse.
-
-    :return (List[Dict[str, Tag]]): A list of dictionaries where each dictionary represents a row in the table.
-    The dictionary keys are the column headers and the values are the corresponding table cells.
-    """
-    columns = list([x.string for x in table.thead.tr.find_all("th")])
-    rows = []
-
-    for row in table.tbody.find_all("tr"):
-        values = row.find_all("td")
-
-        if len(values) != len(columns):
-            raise AnimeFLVParseError("Don't match values size with columns size")
-
-        rows.append({h: x for h, x in zip(columns, values)})
-
-    return rows

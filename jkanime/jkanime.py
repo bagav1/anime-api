5dc93798e25811e670f63a477ca8197fbf4567bc
Bryan Garcia <bragaru@hotmail.es>
2024-08-21 22:49:32 -0500

    Enhance JKAnime with new features for anime tracking
    Add methods to retrieve the latest animes, episodes, and schedules from the JKAnime site. This improves user experience by providing up-to-date information directly from the source, streamlining access to trending content and upcoming releases.
    
    Refactor existing code for clarity and maintainability, while removing unnecessary imports. This cleanup helps ensure a more efficient and readable codebase.

--- jkanime/jkanime.py
+++ jkanime/jkanime.py
@@ -1,29 +1,18 @@
-import base64
 import json
 import re
 from types import TracebackType
-from typing import Dict, List, Optional, Type, Union
-from urllib.parse import unquote, urlencode
+from typing import List, Optional, Type
 
 import cloudscraper
-import requests
-from bs4 import BeautifulSoup, ResultSet, Tag
+from bs4 import BeautifulSoup
 
-# from animeflv.exception import AnimeFLVParseError
-# from animeflv.schema import (
-#     AnimeInfo,
-#     AnimeShortInfo,
-#     DownloadLinkInfo,
-#     EpisodeFormat,
-#     EpisodeInfo,
-#     ListAnime,
-# )
-from jkanime.utils import parse_table, removeprefix, safe_strip
+from jkanime.utils import removeprefix, safe_strip
 
 BASE_URL = "https://jkanime.net"
 DIRECTORY_URL = f"{BASE_URL}/directorio/"
 SEARCH_URL = f"{BASE_URL}/buscar/"
 PAGINATION_EP = f"{BASE_URL}/ajax/pagination_episodes/"
+SCHEDULE_URL = f"{BASE_URL}/horario/"
 
 
 class JKAnime(object):
@@ -31,7 +20,7 @@ class JKAnime(object):
         session = kwargs.get("session", None)
         self._scraper = cloudscraper.create_scraper(
             session,
-            browser={"browser": "chrome", "platform": "windows", "desktop": True}
+            browser={"browser": "chrome", "platform": "windows", "desktop": True},
         )
 
     def close(self) -> None:
@@ -102,6 +91,74 @@ class JKAnime(object):
 
         return {"current_page": page, "last_page": last_page, "data": animes}
 
+    def get_latest_animes(self):
+        url = BASE_URL
+
+        response = self._scraper.get(url)
+        soup = BeautifulSoup(response.text, "lxml")
+
+        elements = soup.select("section.contenido div.trending__anime div.anime__item")
+
+        animes = []
+        for element in elements:
+            information = {
+                "id": removeprefix(element.select_one("div.anime__item__text a").get("href"), BASE_URL).replace("/", ""),
+                "title": safe_strip(element.select_one("div.anime__item__text a").text),
+                "poster": element.select_one("div.anime__item__pic")["data-setbg"],
+                "type": safe_strip(element.select_one("div.anime__item__text ul li.anime").text),
+                "status": safe_strip(element.select_one("div.anime__item__text ul li:first-child").text),
+                "synopsis": None,
+            }
+
+            animes.append(information)
+
+        return animes
+
+    def get_latest_episodes(self):
+        response = self._scraper.get(BASE_URL)
+        soup = BeautifulSoup(response.text, "lxml")
+
+        elements = soup.select("section.hero div.listadoanime-home a.bloqq")
+
+        episodes = []
+        for element in elements:
+            anime, _, id = removeprefix(element["href"], BASE_URL)[1:-1].rpartition("/")
+            information = {
+                "id": id,
+                "anime": anime,
+                "image": element.select_one("div.anime__sidebar__comment__item__pic img").get("src"),
+            }
+            episodes.append(information)
+
+        return episodes
+
+    def get_schedule(self):
+        response = self._scraper.get(SCHEDULE_URL)
+        soup = BeautifulSoup(response.text, "lxml")
+
+        days = soup.select("section.contenido div.semana:not(div.filtro)")
+
+        schedule = []
+        for day in days:
+            elements = day.select("div.cajas div.box")
+            animes = []
+            for element in elements:
+                episode_id = re.findall(r"\d+", element.select_one("div.last span").text)[0]
+                anime_id = removeprefix(element.select_one("a").get("href"), BASE_URL).replace("/", "")
+                information = {
+                    "id": anime_id,
+                    "title": safe_strip(element.select_one("a").text),
+                    "image": element.select_one("div.boxx img").get("src"),
+                    "last_episode": {
+                        "id": episode_id,
+                        "date": safe_strip(element.select_one("div.last time").text),
+                    },
+                }
+                animes.append(information)
+
+            schedule.append({"day": safe_strip(day.select_one("h2").text), "animes": animes})
+
+        return schedule
 
     def get_anime_info(self, id: str):
         url = f"{BASE_URL}/{id}"
@@ -120,7 +177,7 @@ class JKAnime(object):
         languages = anime_details[4].text.split(":")[1:][0].split(", ")
 
         information = {
-            "id": container.select_one("div.anime__details__title h3").text,
+            "id": id,
             "unique_id": container.select_one("div#guardar-anime").get("data-anime"),
             "title": safe_strip(container.select_one("div.anime__details__title h3").text),
             "alt_title": safe_strip(container.select_one("div.anime__details__title span").text),
@@ -143,17 +200,22 @@ class JKAnime(object):
         episodes = []
 
         for i in range(len(pagination_ep)):
-            id = information["unique_id"]
-            resp=self._scraper.get(f"{PAGINATION_EP}/{id}/{i + 1}", headers={"Referer": BASE_URL})
+            resp = self._scraper.get(
+                f"{PAGINATION_EP}/{information['unique_id']}/{i + 1}",
+                headers={"Referer": BASE_URL},
+            )
             resp.raise_for_status()
 
-            episodes.extend([
-                {
-                    "id": e["number"],
-                    "title": e["title"],
-                    "image": "https://cdn.jkdesu.com/assets/images/animes/video/image_thumb/"+ e["image"],
-                } for e in resp.json()
-            ])
+            episodes.extend(
+                [
+                    {
+                        "id": e["number"],
+                        "anime": id,
+                        "image": "https://cdn.jkdesu.com/assets/images/animes/video/image_thumb/" + e["image"],
+                    }
+                    for e in resp.json()
+                ]
+            )
 
         information["episodes"] = episodes
 
@@ -165,38 +227,34 @@ class JKAnime(object):
         response = self._scraper.get(url, headers={"Referer": BASE_URL})
         soup = BeautifulSoup(response.text, "lxml")
 
-
         iframe_urls = []
         for script in soup.find_all("script"):
             contents = str(script)
             remote = BASE_URL
 
-
-
-            pattern = r'video\[\d+\]\s*=.*?<iframe.*?<\/iframe>'
+            pattern = r"video\[\d+\]\s*=.*?<iframe.*?<\/iframe>"
             matches = re.findall(pattern, contents, re.DOTALL)
             for match in matches:
-                src_pattern = r'src=[\"\']([^\"\']+)[\"\']'
+                src_pattern = r"src=[\"\']([^\"\']+)[\"\']"
                 src_matches = re.findall(src_pattern, match)
-                iframe_urls.extend([ remote + src for src in src_matches ])
+                iframe_urls.extend([remote + src for src in src_matches])
 
-            pattern_data_ep = r'var servers\s*=\s*(\[\{.*?\}\])'
+            pattern_data_ep = r"var servers\s*=\s*(\[\{.*?\}\])"
             match_data_ep = re.search(pattern_data_ep, contents)
 
             if match_data_ep:
                 caps = json.loads(match_data_ep.group(1))
                 for cap in caps:
-                    if cap['server'] != 'Mediafire':
+                    if cap["server"] != "Mediafire":
                         iframe_urls.append(f"{remote}/c1.php?u={cap['remote']}&s={cap['server'].lower()}")
 
         urls = []
-        src_stream = ['https://jkanime.net/stream/', 'https://moodle1.playmudos.com']
+        src_stream = ["https://jkanime.net/stream/", "https://moodle1.playmudos.com"]
         for url in iframe_urls:
             resp = self._scraper.get(url, headers={"Referer": BASE_URL})
             urls.append(safe_strip(self.__stream_url(resp.text, src_stream)))
 
         return urls
-
 
     def get_links(self, id: str, episode: int = 1):
         url = f"{BASE_URL}/{id}/{episode}"
@@ -204,15 +262,14 @@ class JKAnime(object):
         response = self._scraper.get(url, headers={"Referer": BASE_URL})
         soup = BeautifulSoup(response.text, "lxml")
 
-
         urls = []
         for script in soup.find_all("script"):
             contents = str(script)
             remote = BASE_URL
 
-            pattern_remote = r'var remote\s*=\s*\'([^\']+)\''
+            pattern_remote = r"var remote\s*=\s*\'([^\']+)\'"
             match_remote = re.search(pattern_remote, contents)
-            pattern_data_ep = r'var servers\s*=\s*(\[\{.*?\}\])'
+            pattern_data_ep = r"var servers\s*=\s*(\[\{.*?\}\])"
             match_data_ep = re.search(pattern_data_ep, contents)
 
             if match_remote and match_data_ep:
@@ -223,11 +280,10 @@ class JKAnime(object):
 
         return urls
 
+    def __stream_url(self, html_content: str, hostnames: List[str]):
+        soup = BeautifulSoup(html_content, "lxml")
 
-    def __stream_url(self, html_content:str, hostnames: List[str]):
-        soup = BeautifulSoup(html_content, 'lxml')
-
-        dplayer_pattern = r'DPlayer\({.*?}\);'
+        dplayer_pattern = r"DPlayer\({.*?}\);"
         matches = re.findall(dplayer_pattern, html_content, re.DOTALL)
         for match in matches:
             for hostname in hostnames:
@@ -235,7 +291,7 @@ class JKAnime(object):
                 if url_match:
                     return url_match.group(0)
 
-        for script in soup.find_all('script'):
+        for script in soup.find_all("script"):
             script_content = script.string
             if script_content:
                 for hostname in hostnames:
@@ -243,7 +299,7 @@ class JKAnime(object):
                     if match:
                         return match.group(0)
 
-        for tag in soup.find_all(['iframe','source']):
-            return tag.get('src')
+        for tag in soup.find_all(["iframe", "source"]):
+            return tag.get("src")
 
         return None
